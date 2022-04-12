fc75038e2ef6e369bd1a6d74f19f2f299d9a704d
Yazdan <yzdn.jt@gmail.com>
2022-04-12 17:40:59 +0430

    str and repr methods are added to the Board class

--- Board.py
+++ Board.py
@@ -1,14 +1,40 @@
-from Pieces import Piece
-
-
 class Board:
-    GAME_BOARD = [[Piece] * 2] * 2
+    ROWS = 10
+    COLS = 10
+    GAME_BOARD = [[None] * 2 * COLS] * 2 * ROWS
 
     def __init__(self):
-        self.a = 3
+        pass
 
+    def __str__(self):
+        res = " "
+        for j in range(Board.COLS):
+            res += 2 * "_" + 4 * " "
+        res += "\n"
+
+        for i in range(Board.ROWS):
+            for j in range(Board.COLS):
+                #if i % 2 == 0:
+                 #   pass
+                res += "/" + 2 * " " + "\\" + 2 * "_"
+
+            res += "\n"
+
+            for j in range(Board.COLS):
+                res += "\\" + 2 * "_" + "/" + 2 * " "
+            res += "\n"
+
+        return res
 
     def __repr__(self):
-        print(Board.GAME_BOARD)
+        res = ""
+        for i in Board.GAME_BOARD:
+            res += str(i) + "\n"
+
+        return res
 
 
+# -------------------------------
+b = Board()
+print(b)
+print(repr(b))
