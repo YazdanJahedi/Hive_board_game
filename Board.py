62227e539bd50b8e43e7bae673d78136f1295995
Yazdan <yzdn.jt@gmail.com>
2022-04-12 19:03:22 +0430

    labeling for str method in Board class is done

--- Board.py
+++ Board.py
@@ -2,46 +2,45 @@ from Pieces import Piece
 
 
 class Board:
-    ROWS = 5
+    ROWS = 6
     COLS = 5
     GAME_BOARD = [[None] * 2 * COLS] * 2 * ROWS
 
     def __init__(self):
+        # This part is written just for testing...
+        # Board.GAME_BOARD[0][0] = 1
         pass
 
     def __str__(self):
         res = " "
         for j in range(Board.COLS):
-            res += 2 * "_" + 4 * " "
+            res += 3 * "_" + 5 * " "
         res += "\n"
 
         for i in range(Board.ROWS):
             for j in range(Board.COLS):
-                piece_label = "  "
+                piece_label = 3 * " "
 
-                if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 0 and j % 2 == 0):
-                    piece = Board.GAME_BOARD[i][j]
-                    if piece is None:
-                        piece_label = "N "
-                    # elif isinstance(piece, Piece):
-                    # piece_label = piece.name[0] + piece.color
-                    # pass
+                piece = Board.GAME_BOARD[2 * i][2 * j]
+                if piece is None:
+                    piece_label = "NON"
+                elif isinstance(piece, Piece):
+                    piece_label = piece.name[0] + piece.color
 
-                res += "/" + piece_label + "\\" + 2 * "_"
+                res += "/" + piece_label + "\\" + 3 * "_"
 
             res += "\n"
 
             for j in range(Board.COLS):
-                piece_label = "  "
+                piece_label = 3 * " "
 
-                if (i % 2 == 0 and j % 2 == 0) or (i % 2 == 0 and j % 2 == 0):
-                    piece = Board.GAME_BOARD[i][j]
-                    if piece is None:
-                        piece_label = "N "
-                    # elif isinstance(piece, Piece):
-                    # piece_label = piece.name[0] + piece.color
-                    # pass
-                res += "\\" + 2 * "_" + "/" + piece_label
+                piece = Board.GAME_BOARD[2 * i + 1][2 * j + 1]
+                if piece is None:
+                    piece_label = "NON"
+                elif isinstance(piece, Piece):
+                    piece_label = piece.name[0] + piece.color
+
+                res += "\\" + 3 * "_" + "/" + piece_label
 
             res += "\n"
 
