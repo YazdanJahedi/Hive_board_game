c6dbe4e3f3891a63c47865d401566d9ac3e99d75
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-13 08:52:21 +0430

    add methods to Board

--- Board.py
+++ Board.py
@@ -1,4 +1,4 @@
-from Pieces import Piece
+from Pieces.Piece import Piece
 
 colors = {
     'Black': '\u001b[30m',
@@ -16,15 +16,19 @@ colors = {
 class Board:
     ROWS = 5
     COLS = 5
-    GAME_BOARD = []
+
+    # {tuple Piece -> player}
 
     def __init__(self):
+        self.GAME_BOARD = []
+        self.full_positions = {}
         for i in range(2 * Board.ROWS):
-            Board.GAME_BOARD.append([None] * 2 * Board.COLS)
-        pass
+            self.GAME_BOARD.append([None] * 2 * Board.COLS)
 
     def __str__(self, **kwargs):
         selects = kwargs.get('selects', {})
+        for piece, player in self.full_positions.items():
+            selects[(piece.pos['x'], piece.pos['y'])] = self.full_positions[piece]
         res = " "
         for j in range(Board.COLS):
             res += 3 * "_" + 5 * " "
@@ -34,7 +38,7 @@ class Board:
             for j in range(Board.COLS):
                 piece_label = 3 * " "
 
-                piece = Board.GAME_BOARD[2 * i][2 * j]
+                piece = self.GAME_BOARD[2 * i][2 * j]
                 if piece is None and (i, j) in selects.keys():
                     color_name = selects.get((i, j))
                     color = colors.get(color_name)
@@ -52,7 +56,7 @@ class Board:
             for j in range(Board.COLS):
                 piece_label = 3 * " "
 
-                piece = Board.GAME_BOARD[2 * i + 1][2 * j + 1]
+                piece = self.GAME_BOARD[2 * i + 1][2 * j + 1]
                 if piece is None:
                     piece_label = "NON"
                 # elif isinstance(piece, Piece):
@@ -64,18 +68,50 @@ class Board:
 
         return res
 
+    def contains_enemy(self, list_of_cells, player):
+        for cell in list_of_cells:
+            if isinstance(cell, tuple):
+                continue
+            if cell.player != player:
+                return True
+        return False
+
     def __repr__(self):
         res = ""
-        for i in Board.GAME_BOARD:
+        for i in self.GAME_BOARD:
             res += str(i) + "\n"
 
         return res
+
+    def get_possible_insertions(self, player):
+        if len(self.full_positions) == 0:
+            return {(self.ROWS // 2, self.COLS // 2)}
+        if len(self.full_positions) == 1:
+            return set(k for k in list(self.full_positions.keys())[0].get_neighbors().values() if k is not None)
+        output = set()  # set of tuples of positions
+        for piece, _ in self.full_positions.items():
+            neighbors = piece.get_neighbors()
+            for neighbor in neighbors.values():
+                # neighbor is tuple of x,y if None else Piece
+                if isinstance(neighbor, Piece):
+                    continue
+                fake_piece = Piece('')
+                fake_piece.pos['x'] = neighbor[0]
+                fake_piece.pos['y'] = neighbor[1]
+                fake_piece.board = self
+                if not self.contains_enemy(fake_piece.get_neighbors().values(), player):
+                    output.add(neighbor)
+        return output
+
+    def add_piece(self, pos, piece, color):
+        self.GAME_BOARD[pos[0]][pos[1]] = piece
+        self.full_positions[piece] = color
 
 
 # -------------------------------
 if __name__ == '__main__':
     b = Board()
-    b.GAME_BOARD[0][0] = 9
+    b.GAME_BOARD[1][1] = 9
     print(b.GAME_BOARD[0][0])
     print(b.__str__(selects={(2, 3): 'Red', (0, 4): 'Blue'}))
     # print(repr(b))
