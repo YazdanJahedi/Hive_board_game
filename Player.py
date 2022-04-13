6db42d6321b9600c311670de08375e2d23b9654e
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-13 08:51:06 +0430

    add a temp field to test run

--- Player.py
+++ Player.py
@@ -5,5 +5,12 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.pieces = [Pieces] * 6
+        self.pieces = {
+            'QB': 1,
+            'S': 2,
+            'B': 2,
+            'G': 3,
+            'A': 3,
+        }
         self.is_won = False
         self.score = 0
