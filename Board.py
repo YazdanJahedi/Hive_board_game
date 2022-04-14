9e3c5762dd85104e71b7e2410567cdf0f6763c58
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-14 11:19:20 +0430

    bug fixes and color print add to TODO remove list.

--- Board.py
+++ Board.py
@@ -25,6 +25,7 @@ class Board:
         self.full_positions = {}  # {tuple Piece -> player}
 
     def __str__(self, **kwargs):
+        # TODO: remove colored print
         selects = kwargs.get('selects', {})
         for piece, player in self.full_positions.items():
             selects[(piece.pos['x'], piece.pos['y'])] = self.full_positions[piece]
@@ -38,11 +39,11 @@ class Board:
                 piece_label = 3 * " "
 
                 piece = self.GAME_BOARD[2 * i][2 * j]
-                if piece is None and (i, j) in selects.keys():
-                    color_name = selects.get((i, j))
-                    color = colors.get(color_name)
-                    piece_label = color + "NON" + colors.get('Normal')
-                elif piece is None:
+                # if piece is None and (i, j) in selects.keys():
+                #     color_name = selects.get((i, j))
+                #     color = colors.get(color_name)
+                #     piece_label = color + "NON" + colors.get('Normal')
+                if piece is None:
                     piece_label = "NON"
                 elif isinstance(piece, Piece):
                     piece_label = piece.name[0] + "-" + piece.color[0]
@@ -68,7 +69,7 @@ class Board:
 
     def contains_enemy(self, list_of_cells, player):
         for cell in list_of_cells:
-            if isinstance(cell, tuple):
+            if isinstance(cell, tuple) or isinstance(cell, str):
                 continue
             if cell.player != player:
                 return True
@@ -83,7 +84,7 @@ class Board:
 
     def get_possible_insertions(self, player):
         if len(self.full_positions) == 0:
-            return {(self.ROWS // 2, self.COLS // 2)}
+            return {(self.ROWS - 1, self.COLS - 1)}
         if len(self.full_positions) == 1:
             return set(k for k in list(self.full_positions.keys())[0].get_neighbors().values() if k is not None)
         output = set()  # set of tuples of positions
@@ -93,10 +94,14 @@ class Board:
                 # neighbor is tuple of x,y if None else Piece
                 if isinstance(neighbor, Piece):
                     continue
+                if neighbor == "ERROR":
+                    continue
                 fake_piece = Piece('')
                 fake_piece.pos['x'] = neighbor[0]
                 fake_piece.pos['y'] = neighbor[1]
                 fake_piece.board = self
+                if isinstance(neighbor[0], str):
+                    print()
                 if not self.contains_enemy(fake_piece.get_neighbors().values(), player):
                     output.add(neighbor)
         return output
@@ -104,6 +109,9 @@ class Board:
     def add_piece(self, pos, piece, color):
         self.GAME_BOARD[pos[0]][pos[1]] = piece
         self.full_positions[piece] = color
+
+    def is_position_in_board(self, position):
+        return 0 <= position[0] < self.ROWS * 2 and 0 <= position[1] < self.COLS * 2
 
 
 # -------------------------------
--- Pieces/Piece.py
+++ Pieces/Piece.py
@@ -13,27 +13,39 @@ class Piece:
 
     # TODO: handle possible index out of bound exception
     def get_n(self):
-        return self.board.GAME_BOARD[self.pos["x"] - 2][self.pos["y"]]
+        if self.board.is_position_in_board((self.pos["x"] - 2,self.pos["y"])):
+            return self.board.GAME_BOARD[self.pos["x"] - 2][self.pos["y"]]
+        return "ERROR"
 
     def get_s(self):
-        return self.board.GAME_BOARD[self.pos["x"] + 2][self.pos["y"]]
+        if self.board.is_position_in_board((self.pos["x"] + 2,self.pos["y"])):
+            return self.board.GAME_BOARD[self.pos["x"] + 2][self.pos["y"]]
+        return "ERROR"
 
     def get_nw(self):
-        return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] - 1]
+        if self.board.is_position_in_board((self.pos["x"] - 1,self.pos["y"] - 1)):
+            return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] - 1]
+        return "ERROR"
 
     def get_ne(self):
-        return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] + 1]
+        if self.board.is_position_in_board((self.pos["x"] - 1,self.pos["y"] + 1)):
+            return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] + 1]
+        return "ERROR"
 
     def get_sw(self):
-        return self.board.GAME_BOARD[self.pos["x"] + 1][self.pos["y"] - 1]
+        if self.board.is_position_in_board((self.pos["x"] + 1, self.pos["y"] - 1)):
+            return self.board.GAME_BOARD[self.pos["x"] + 1][self.pos["y"] - 1]
+        return "ERROR"
 
     def get_se(self):
-        return self.board.GAME_BOARD[self.pos["x"] + 1][self.pos["y"] + 1]
+        if self.board.is_position_in_board((self.pos["x"] + 1, self.pos["y"] + 1)):
+            return self.board.GAME_BOARD[self.pos["x"] + 1][self.pos["y"] + 1]
+        return "ERROR"
 
     def get_neighbors(self):
         return {
-            'n': self.get_n() if self.get_n() is not None else (self.pos["x"] - 2, self.pos["y"]),
-            'w': self.get_s() if self.get_s() is not None else (self.pos["x"] + 2, self.pos["y"]),
+            'n':  self.get_n() if self.get_n() is not None else (self.pos["x"] - 2, self.pos["y"]),
+            'w':  self.get_s() if self.get_s() is not None else (self.pos["x"] + 2, self.pos["y"]),
             'nw': self.get_nw() if self.get_nw() is not None else (self.pos["x"] - 1, self.pos["y"] - 1),
             'ne': self.get_ne() if self.get_ne() is not None else (self.pos["x"] - 1, self.pos["y"] + 1),
             'sw': self.get_sw() if self.get_sw() is not None else (self.pos["x"] + 1, self.pos["y"] - 1),
