a3c8f0870629172b6139a518ccc8330d0743bda0
Yazdan <yzdn.jt@gmail.com>
2022-04-15 18:56:15 +0430

    movement in tested pygame

--- Test.py
+++ Test.py
@@ -13,12 +13,13 @@ pygame.display.set_icon(icon)
 
 # adding image
 img = pygame.image.load("images/spider.png")
-imgX = 0
-imgY = 0
+imgX = 400
+imgY = 400
+imgX_changer = 0
+imgY_changer = 0
 
-
-def img_display():
-    screen.blit(img, (imgX, imgY))
+def img_display(x, y):
+    screen.blit(img, (x, y))
 
 
 # game loop (screen loop)
@@ -31,7 +32,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    img_display()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("right key is pressed")
+                imgX_changer += 0.3
+            if event.key == pygame.K_LEFT:
+                print("left key is pressed")
+                imgX_changer -= 0.3
+            if event.key == pygame.K_UP:
+                print("up key is pressed")
+                imgY_changer -= 0.3
+            if event.key == pygame.K_DOWN:
+                print("down key is pressed")
+                imgY_changer += 0.3
+        if event.type == pygame.KEYUP:
+            print("key is released!")
+            imgX_changer = 0
+            imgY_changer = 0
+
+    imgX += imgX_changer
+    imgY += imgY_changer
+    img_display(imgX, imgY)
     pygame.display.update()
 
 # 30:00
