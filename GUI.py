6d586f16bc060d4b599e49a5a9348622ea2ade4a
Yazdan <yzdn.jt@gmail.com>
2022-04-17 00:30:14 +0430

    pieces are shown GUI

--- GUI.py
+++ GUI.py
@@ -6,7 +6,7 @@ from Board import Board
 pygame.init()
 
 # Set screen size
-screen_size = screen_width, screen_height = (1000, 600)
+screen_size = screen_width, screen_height = (1100, 650)
 screen = pygame.display.set_mode(screen_size)
 
 # Title and icon
@@ -49,42 +49,79 @@ def show_board():
                 if board[i][2 * j + 1] == 1:
                     cell_image = pygame.image.load("images/Hexagonal_blue.png")
 
-
             # Scale the image
             cell_image = pygame.transform.scale(cell_image, DEFAULT_CELL_SIZE)
             screen.blit(cell_image, (current_x, current_y))
             current_x += (3 / 2) * cell_width
         current_y += cell_height / 2
 
-def show_pieces():
+
+# index: queen_bee  /  spider  /  ant  /  grasshopper  /  beetle
+white_pieces_image = []
+black_pieces_image = []
+
+
+def init_pieces_image():
     # load image
     queen_bee_white = pygame.image.load("images/queen_bee_white.png")
-    screen.blit(queen_bee_white, (,))
-    queen_bee_black = pygame.image.load("images/queen_bee_black.png")
-    screen.blit(queen_bee_white, (,))
+    # Scale the image
+    queen_bee_white = pygame.transform.scale(queen_bee_white, DEFAULT_CELL_SIZE)
+    white_pieces_image.append(queen_bee_white)
 
     spider_white = pygame.image.load("images/spider_white.png")
-    screen.blit(queen_bee_white, (,))
-    spider_black = pygame.image.load("images/spider_black.png")
-    screen.blit(queen_bee_white, (,))
+    spider_white = pygame.transform.scale(spider_white, DEFAULT_CELL_SIZE)
+    white_pieces_image.append(spider_white)
 
     ant_white = pygame.image.load("images/ant_white.png")
-    screen.blit(queen_bee_white, (,))
-    ant_black = pygame.image.load("images/ant_black.png")
-    screen.blit(queen_bee_white, (,))
+    ant_white = pygame.transform.scale(ant_white, DEFAULT_CELL_SIZE)
+    white_pieces_image.append(ant_white)
 
     grasshopper_white = pygame.image.load("images/grasshopper_white.png")
-    screen.blit(queen_bee_white, (,))
+    grasshopper_white = pygame.transform.scale(grasshopper_white, DEFAULT_CELL_SIZE)
+    white_pieces_image.append(grasshopper_white)
+
+    beetle_white = pygame.image.load("images/insect_white.png")
+    beetle_white = pygame.transform.scale(beetle_white, DEFAULT_CELL_SIZE)
+    white_pieces_image.append(beetle_white)
+    # - - - - - - - - - - - - - - - - - - -
+    queen_bee_black = pygame.image.load("images/queen_bee_black.png")
+    queen_bee_black = pygame.transform.scale(queen_bee_black, DEFAULT_CELL_SIZE)
+    black_pieces_image.append(queen_bee_black)
+
+    spider_black = pygame.image.load("images/spider_black.png")
+    spider_black = pygame.transform.scale(spider_black, DEFAULT_CELL_SIZE)
+    black_pieces_image.append(spider_black)
+
+    ant_black = pygame.image.load("images/ant_black.png")
+    ant_black = pygame.transform.scale(ant_black, DEFAULT_CELL_SIZE)
+    black_pieces_image.append(ant_black)
+
     grasshopper_black = pygame.image.load("images/grasshopper_black.png")
-    screen.blit(queen_bee_white, (,))
+    grasshopper_black = pygame.transform.scale(grasshopper_black, DEFAULT_CELL_SIZE)
+    black_pieces_image.append(grasshopper_black)
 
-    insect_white = pygame.image.load("images/insect_white.png")
-    screen.blit(queen_bee_white, (,))
-    insect_black = pygame.image.load("images/insect_black.png")
-    screen.blit(queen_bee_white, (,))
+    beetle_black = pygame.image.load("images/insect_black.png")
+    beetle_black = pygame.transform.scale(beetle_black, DEFAULT_CELL_SIZE)
+    black_pieces_image.append(beetle_black)
 
-    pass
+
+def show_pieces():
+    starting_x = 800
+    starting_y = 50
+    for i in white_pieces_image:
+        screen.blit(i, (starting_x, starting_y))
+        starting_x += 60
+
+    starting_x = 800
+    starting_y = 150
+    for i in black_pieces_image:
+        screen.blit(i, (starting_x, starting_y))
+        starting_x += 60
+
+
 # -------------------------------
+init_pieces_image()
+
 # Event loop
 running = True
 while running:
