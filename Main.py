57c1b60d66e7a1c3b7eed43812f7a5b6b470eaf7
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-18 22:12:50 +0430

    add input movement command

--- Main.py
+++ Main.py
@@ -15,31 +15,39 @@ while True:
     in_turn_player = p1 if turn % 2 == 0 else p2
     color = 'Blue' if turn % 2 == 0 else 'Red'
     print(board)
-    print('You have these pieces:')
-    print(in_turn_player.pieces)
-    piece = input('Choose one exist piece with enter id. ex) QB\n')
-    piece_num = in_turn_player.pieces.get(piece, -1)
-    if piece_num <= 0:
-        print("You don't have this piece")
-        continue
-    piece_obj = None
-    if piece == 'QB':
-        piece_obj = QueenBee(color)
-    elif piece == 'S':
-        piece_obj = Spider(color)
-    elif piece == 'B':
-        piece_obj = Beetle(color)
-    elif piece == 'G':
-        piece_obj = Grasshopper(color)
-    elif piece == 'A':
-        piece_obj = Ant(color)
-    piece_obj.board = board
-    in_turn_player.pieces[piece] = piece_num - 1
-    print(board.get_possible_insertions(in_turn_player))
-    # validate pos
-    pos = list(map(int, input('enter position in form of x y. ex) 5 4\n').split()))
-    piece_obj.pos['x'] = pos[0]
-    piece_obj.pos['y'] = pos[1]
-    piece_obj.player = in_turn_player
-    board.add_piece(pos, piece_obj, color)
+    command = input('move or insert?\n')
+    if command == 'insert':
+        print('You have these pieces:')
+        print(in_turn_player.pieces)
+        piece = input('Choose one exist piece with enter id. ex) QB\n')
+        piece_num = in_turn_player.pieces.get(piece, -1)
+        if piece_num <= 0:
+            print("You don't have this piece")
+            continue
+        piece_obj = None
+        if piece == 'QB':
+            piece_obj = QueenBee(color)
+        elif piece == 'S':
+            piece_obj = Spider(color)
+        elif piece == 'B':
+            piece_obj = Beetle(color)
+        elif piece == 'G':
+            piece_obj = Grasshopper(color)
+        elif piece == 'A':
+            piece_obj = Ant(color)
+        piece_obj.board = board
+        in_turn_player.pieces[piece] = piece_num - 1
+        print(board.get_possible_insertions(in_turn_player))
+        # validate pos
+        pos = list(map(int, input('enter position in form of x y. ex) 5 4\n').split()))
+        piece_obj.pos['x'] = pos[0]
+        piece_obj.pos['y'] = pos[1]
+        piece_obj.player = in_turn_player
+        board.add_piece(pos, piece_obj, color)
+    else:
+        x, y = map(int, input('insert x y to move it:\n').split())
+        to_move_piece = board.GAME_BOARD[x][y]
+        print(f"possible destinations: {to_move_piece.possible_movements()}")
+        x, y = map(int, input('enter destination x y:\n').split())
+        board.move(to_move_piece, (x, y))
     turn += 1
