5601d1a882d4e3d6b173318eb851ef12de87f591
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-18 22:12:00 +0430

    add get each neighbor position

--- Pieces/Piece.py
+++ Pieces/Piece.py
@@ -7,28 +7,27 @@ class Piece:
         self.board = None
         self.player = None
 
-
     def place(self, x, y):
         self.pos = {"x": x, "y": y}
 
     # TODO: handle possible index out of bound exception
     def get_n(self):
-        if self.board.is_position_in_board((self.pos["x"] - 2,self.pos["y"])):
+        if self.board.is_position_in_board((self.pos["x"] - 2, self.pos["y"])):
             return self.board.GAME_BOARD[self.pos["x"] - 2][self.pos["y"]]
         return "ERROR"
 
     def get_s(self):
-        if self.board.is_position_in_board((self.pos["x"] + 2,self.pos["y"])):
+        if self.board.is_position_in_board((self.pos["x"] + 2, self.pos["y"])):
             return self.board.GAME_BOARD[self.pos["x"] + 2][self.pos["y"]]
         return "ERROR"
 
     def get_nw(self):
-        if self.board.is_position_in_board((self.pos["x"] - 1,self.pos["y"] - 1)):
+        if self.board.is_position_in_board((self.pos["x"] - 1, self.pos["y"] - 1)):
             return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] - 1]
         return "ERROR"
 
     def get_ne(self):
-        if self.board.is_position_in_board((self.pos["x"] - 1,self.pos["y"] + 1)):
+        if self.board.is_position_in_board((self.pos["x"] - 1, self.pos["y"] + 1)):
             return self.board.GAME_BOARD[self.pos["x"] - 1][self.pos["y"] + 1]
         return "ERROR"
 
@@ -42,10 +41,29 @@ class Piece:
             return self.board.GAME_BOARD[self.pos["x"] + 1][self.pos["y"] + 1]
         return "ERROR"
 
+    def get_n_pos(self):
+        return self.pos["x"] - 2, self.pos["y"]
+
+    def get_s_pos(self):
+        return self.pos["x"] + 2, self.pos["y"]
+
+    def get_nw_pos(self):
+        return self.pos["x"] - 1, self.pos["y"] - 1
+
+    def get_ne_pos(self):
+        return self.pos["x"] - 1, self.pos["y"] + 1
+
+    def get_sw_pos(self):
+        return self.pos["x"] + 1, self.pos["y"] - 1
+
+    def get_se_pos(self):
+        return self.pos["x"] + 1, self.pos["y"] + 1
+
+
     def get_neighbors(self):
         return {
-            'n':  self.get_n() if self.get_n() is not None else (self.pos["x"] - 2, self.pos["y"]),
-            'w':  self.get_s() if self.get_s() is not None else (self.pos["x"] + 2, self.pos["y"]),
+            'n': self.get_n() if self.get_n() is not None else (self.pos["x"] - 2, self.pos["y"]),
+            'w': self.get_s() if self.get_s() is not None else (self.pos["x"] + 2, self.pos["y"]),
             'nw': self.get_nw() if self.get_nw() is not None else (self.pos["x"] - 1, self.pos["y"] - 1),
             'ne': self.get_ne() if self.get_ne() is not None else (self.pos["x"] - 1, self.pos["y"] + 1),
             'sw': self.get_sw() if self.get_sw() is not None else (self.pos["x"] + 1, self.pos["y"] - 1),
@@ -54,4 +72,3 @@ class Piece:
 
     def move(self):
         pass
-
