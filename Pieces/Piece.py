ecc01932678577948104a09da066e9c202f60288
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-20 09:55:03 +0430

    add method to get not null adjacent neighbors

--- Pieces/Piece.py
+++ Pieces/Piece.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 class Piece:
     def __init__(self, color):
         self.name = "piece"
@@ -59,7 +62,6 @@ class Piece:
     def get_se_pos(self):
         return self.pos["x"] + 1, self.pos["y"] + 1
 
-
     def get_neighbors(self):
         return {
             'n': self.get_n() if self.get_n() is not None else (self.pos["x"] - 2, self.pos["y"]),
@@ -69,6 +71,26 @@ class Piece:
             'sw': self.get_sw() if self.get_sw() is not None else (self.pos["x"] + 1, self.pos["y"] - 1),
             'se': self.get_se() if self.get_se() is not None else (self.pos["x"] + 1, self.pos["y"] + 1),
         }
+
+    def get_null_adjacent_neighbors(self, base_piece):
+        output = []
+        neighbors = self.get_neighbors().values()
+        for neighbor in neighbors:
+            if isinstance(neighbor, Piece):
+                continue
+            fake = Piece('')
+            fake.pos = {'x': neighbor[0], 'y': neighbor[1]}
+            fake.board = self.board
+            not_null_neighbors = \
+                reduce(lambda before, n: before + [n] if isinstance(n,Piece) else before, fake.get_neighbors().values(), [])
+            if len(not_null_neighbors) == 1:
+                temp = not_null_neighbors.pop()
+                if temp.pos['x'] != base_piece.pos['x'] or temp.pos['y'] != base_piece.pos['y']:
+                    output.append(neighbor)
+            elif len(not_null_neighbors) > 0:
+                output.append(neighbor)
+
+        return output
 
     def move(self):
         pass
