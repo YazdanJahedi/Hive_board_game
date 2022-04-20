fcf8ccdb60a8f0e69d8f09d79d22b303552c13d9
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-20 09:55:22 +0430

    add movement function to Ant

--- Pieces/Ant.py
+++ Pieces/Ant.py
@@ -2,4 +2,21 @@ from Pieces.Piece import Piece
 
 
 class Ant(Piece):
-    pass
+    def possible_movements(self):
+        output = []
+        open_list = []
+        visited = []
+        open_list.append(self)
+        visited.append(tuple(self.pos.values()))
+        while len(open_list) != 0:
+            this_pos = open_list.pop()
+            output.append(tuple(this_pos.pos.values()))
+            for n in this_pos.get_null_adjacent_neighbors(self):
+                if n not in visited:
+                    fake = Piece('')
+                    fake.pos = {'x': n[0], 'y': n[1]}
+                    fake.board = self.board
+                    visited.append(n)
+                    open_list.append(fake)
+        output.remove(tuple(self.pos.values()))
+        return output
