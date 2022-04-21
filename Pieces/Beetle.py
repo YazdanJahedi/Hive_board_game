d298178b7ab38dee63568cb5e6dd89edf42aaa4b
Mahmood Choopani <choopani.m81@gmail.com>
2022-04-21 12:49:30 +0430

    add movement function to beetle

--- Pieces/Beetle.py
+++ Pieces/Beetle.py
@@ -3,4 +3,5 @@ from Pieces.Piece import Piece
 
 class Beetle(Piece):
     def possible_movements(self):
-        pass
+        return {neighbor if isinstance(neighbor, tuple) else tuple(neighbor.pos.values())
+                for neighbor in filter(lambda x: not isinstance(x, str), self.get_neighbors().values())}
