e4827c9c4413919b80bc4deb5ac09c7789db58f4
Yazdan <yzdn.jt@gmail.com>
2022-05-21 14:45:15 +0430

    heuristic method is added

--- AI.py
+++ AI.py
@@ -1,9 +1,12 @@
 # This file provides some functions for AI part of the game
+from Board import Board
 from Pieces.Ant import Ant
 from Pieces.Beetle import Beetle
 
 # This function provides an estimated heuristic for the game.
 # Using this method, minMax tree can be used in an efficient way!
+from Pieces.Piece import Piece
+
 """"
 This function calculates heuristic for the player1.
 So we should maximize heuristic for player1
@@ -28,13 +31,31 @@ def get_heuristic(board, player1, player2, turn):
         h += 120
 
     h += 50 * get_number_of_inserted_ants(player1)
+    h += 100 * get_number_of_locked_pieces(board, player2)
+    # _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+    if get_number_of_inserted_beetles(player2) != 0:
+        h -= 30
+    if is_queen_bee_inserted(player2):
+        h -= 120
+
+    h -= 50 * get_number_of_inserted_ants(player2)
+    h -= 100 * get_number_of_locked_pieces(board, player1)
 
     return h
 
 
 # this method checks if a piece in the game board can move or not.
 def is_piece_a_bridge(board, piece):
-    pass
+    possible_movements = piece.get_possible_movements()
+    for i in possible_movements:
+        pre_pos = piece.pos
+        board.move(piece, i)
+        if board.is_connected():
+            board.move(piece, pre_pos)
+            return False
+        board.move(piece, pre_pos)
+
+    return True
 
 
 # This method returns number of beetles that player has inserted
@@ -56,5 +77,14 @@ def get_number_of_inserted_ants(player):
     n = 0
     for i in player.inserted_pieces:
         if isinstance(i, Ant):
+            n += 1
+    return n
+
+
+# This method returns number of locked pieces that can not to move, for given player!
+def get_number_of_locked_pieces(board, player):
+    n = 0
+    for i in player.inserted_pieces:
+        if is_piece_a_bridge(board, i):
             n += 1
     return n
